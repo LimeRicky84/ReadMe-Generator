409bf7e35e4c2e6b26cd9c3615f996ff205ca8c0
Ricma <ric.madse@gmail.com>
2022-06-11 14:38:24 -0600

    default files added

---
+++ Develop/index.js
@@ -0,0 +1,25 @@
+const inquirer = require('inquirer')
+const markdown = require('./generateMarkdown.js')
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [
+//  questions to add:  Title of project
+// Description of project
+// Table of contents
+// Installation
+// usage information
+// License (select from list, applies badge to readme)
+// Contributing  -- contribution guidelines
+// Tests -- test instructions
+// Questions
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
